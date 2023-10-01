be3a5f9a7916a65bfd627d827bb9e8dc90a9c244
tend <rydenko.urii@mail.ru>
2023-10-01 07:12:58 +0300

    fds

--- scripts/drawStatus.js
+++ scripts/drawStatus.js
@@ -1,168 +1,168 @@
-const TG = window.Telegram?.WebApp;
+// const TG = window.Telegram?.WebApp;
 
-const errorModal = (message) => {
-  const overlay = document.createElement("div");
-  overlay.classList.add("modal-overlay");
-  overlay.id = "modal-overlay";
-  const header = document.createElement("h2");
-  header.innerText = "Что-то пошло не так..";
-  const wrapper = document.createElement("div");
-  wrapper.appendChild(header);
-  wrapper.classList.add("modal-wrapper");
-  const container = document.createElement("p");
-  container.innerText = message;
-  wrapper.appendChild(container);
-  overlay.appendChild(wrapper);
-  overlay.addEventListener("click", (e) => _onModalDismiss(e, overlay));
-  document.body.appendChild(overlay);
-};
+// const errorModal = (message) => {
+//   const overlay = document.createElement("div");
+//   overlay.classList.add("modal-overlay");
+//   overlay.id = "modal-overlay";
+//   const header = document.createElement("h2");
+//   header.innerText = "Что-то пошло не так..";
+//   const wrapper = document.createElement("div");
+//   wrapper.appendChild(header);
+//   wrapper.classList.add("modal-wrapper");
+//   const container = document.createElement("p");
+//   container.innerText = message;
+//   wrapper.appendChild(container);
+//   overlay.appendChild(wrapper);
+//   overlay.addEventListener("click", (e) => _onModalDismiss(e, overlay));
+//   document.body.appendChild(overlay);
+// };
 
-const fields = [
-  { label: "Task Name:", type: "text", name: "taskName", required: true },
-  { label: "Task Description:", type: "textarea", name: "taskDescription" },
-  {
-    label: "Priority:",
-    type: "select",
-    name: "priority",
-    options: ["Low", "Medium", "High"],
-  },
-  { label: "End Date:", type: "date", name: "endDate" },
-];
+// const fields = [
+//   { label: "Task Name:", type: "text", name: "taskName", required: true },
+//   { label: "Task Description:", type: "textarea", name: "taskDescription" },
+//   {
+//     label: "Priority:",
+//     type: "select",
+//     name: "priority",
+//     options: ["Low", "Medium", "High"],
+//   },
+//   { label: "End Date:", type: "date", name: "endDate" },
+// ];
 
-function toggleDropdown(category) {
-  const dropdown = document.getElementById(category);
+// function toggleDropdown(category) {
+//   const dropdown = document.getElementById(category);
 
-  dropdown.classList.toggle("open");
-}
+//   dropdown.classList.toggle("open");
+// }
 
-const showAddTaskModal = (children) => {
-  const overlay = document.createElement("div");
-  overlay.classList.add("modal-overlay");
-  overlay.id = "modal-overlay";
-  const header = document.createElement("h2");
-  header.innerText = "Создание новой задачи";
-  const wrapper = document.createElement("div");
-  wrapper.appendChild(header);
-  wrapper.classList.add("modal-wrapper");
-  const formContainer = document.createElement("div");
-  formContainer.classList.add("form-container");
-  const form = document.createElement("form");
-  form.classList.add("form-container");
+// const showAddTaskModal = (children) => {
+//   const overlay = document.createElement("div");
+//   overlay.classList.add("modal-overlay");
+//   overlay.id = "modal-overlay";
+//   const header = document.createElement("h2");
+//   header.innerText = "Создание новой задачи";
+//   const wrapper = document.createElement("div");
+//   wrapper.appendChild(header);
+//   wrapper.classList.add("modal-wrapper");
+//   const formContainer = document.createElement("div");
+//   formContainer.classList.add("form-container");
+//   const form = document.createElement("form");
+//   form.classList.add("form-container");
 
-  fields.forEach((field) => {
-    const label = document.createElement("label");
-    label.innerText = field.label;
+//   fields.forEach((field) => {
+//     const label = document.createElement("label");
+//     label.innerText = field.label;
 
-    if (field.type === "textarea") {
-      const textarea = document.createElement("textarea");
-      textarea.name = field.name;
-      form.appendChild(label);
-      form.appendChild(textarea);
-    } else if (field.type === "select") {
-      const select = document.createElement("select");
-      select.name = field.name;
+//     if (field.type === "textarea") {
+//       const textarea = document.createElement("textarea");
+//       textarea.name = field.name;
+//       form.appendChild(label);
+//       form.appendChild(textarea);
+//     } else if (field.type === "select") {
+//       const select = document.createElement("select");
+//       select.name = field.name;
 
-      field.options.forEach((optionText) => {
-        const option = document.createElement("option");
-        option.value = optionText.toLowerCase();
-        option.text = optionText;
-        select.appendChild(option);
-      });
+//       field.options.forEach((optionText) => {
+//         const option = document.createElement("option");
+//         option.value = optionText.toLowerCase();
+//         option.text = optionText;
+//         select.appendChild(option);
+//       });
 
-      form.appendChild(label);
-      form.appendChild(select);
-    } else {
-      const input = document.createElement("input");
-      input.type = field.type;
-      input.name = field.name;
-      if (field.required) {
-        input.required = true;
-      }
-      form.appendChild(label);
-      form.appendChild(input);
-    }
-  });
+//       form.appendChild(label);
+//       form.appendChild(select);
+//     } else {
+//       const input = document.createElement("input");
+//       input.type = field.type;
+//       input.name = field.name;
+//       if (field.required) {
+//         input.required = true;
+//       }
+//       form.appendChild(label);
+//       form.appendChild(input);
+//     }
+//   });
 
-  const submitButton = document.createElement("button");
-  submitButton.type = "submit";
-  submitButton.innerText = "Добавить";
-  form.appendChild(submitButton);
-  formContainer.appendChild(form);
+//   const submitButton = document.createElement("button");
+//   submitButton.type = "submit";
+//   submitButton.innerText = "Добавить";
+//   form.appendChild(submitButton);
+//   formContainer.appendChild(form);
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log(e);
-  });
-  //
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     console.log(e);
+//   });
+//   //
 
-  wrapper.appendChild(formContainer);
-  overlay.appendChild(wrapper);
-  overlay.addEventListener("click", (e) => _onModalDismiss(e, overlay));
-  document.body.appendChild(overlay);
-};
+//   wrapper.appendChild(formContainer);
+//   overlay.appendChild(wrapper);
+//   overlay.addEventListener("click", (e) => _onModalDismiss(e, overlay));
+//   document.body.appendChild(overlay);
+// };
 
-function addTask(e, status) {
-  e.stopPropagation();
-  showAddTaskModal();
-}
-const getter = async (url, body) => {
-  console.log({
-    user_id: TG.initDataUnsafe?.user?.id,
-    ...body,
-  });
-  return await fetch("https://innoglobalhack.site/api/" + url, {
-    method: "POST",
-    body: JSON.stringify({
-      user_id: TG.initDataUnsafe?.user?.id,
-      ...body,
-    }),
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .catch((err) => console.log(err));
-};
+// function addTask(e, status) {
+//   e.stopPropagation();
+//   showAddTaskModal();
+// }
+// const getter = async (url, body) => {
+//   console.log({
+//     user_id: TG.initDataUnsafe?.user?.id,
+//     ...body,
+//   });
+//   return await fetch("https://innoglobalhack.site/api/" + url, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       user_id: TG.initDataUnsafe?.user?.id,
+//       ...body,
+//     }),
+//     headers: { "Content-Type": "application/json" },
+//   })
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .catch((err) => console.log(err));
+// };
 
-getter("get_columns_by_user_id")
-  // .then((res) =>
-  //   res.ok && 0 ? res.json() : ["inWork", "done", "planned", "closed"]
-  // )
-  .then((data) => {
-    console.log("RESPONSE: ", data);
-    window.fetchedData = data;
-    const container = document.querySelector(".tasks");
-    let html = "";
-    data.forEach(({ name }) => {
-      html += `
-      <div class="dropdown category" id="${createValidVariableName(name)}">
-      <div class="dropdown-btn" onclick="toggleDropdown('${createValidVariableName(
-        name
-      )}')">
-        <p>${name}</p>
-        <div class="addTask" onclick="addTask(event)">        
-          <img src="/assets/icons/add.svg" alt="Добавить" />
-        </div>
-      </div>
-      <div class="dropdown-content"></div>
-    </div>
-      `;
-    });
-    container.innerHTML = html;
-  })
-  .catch((err) => errorModal(err.message));
+// getter("get_columns_by_user_id")
+//   // .then((res) =>
+//   //   res.ok && 0 ? res.json() : ["inWork", "done", "planned", "closed"]
+//   // )
+//   .then((data) => {
+//     console.log("RESPONSE: ", data);
+//     window.fetchedData = data;
+//     const container = document.querySelector(".tasks");
+//     let html = "";
+//     data.forEach(({ name }) => {
+//       html += `
+//       <div class="dropdown category" id="${createValidVariableName(name)}">
+//       <div class="dropdown-btn" onclick="toggleDropdown('${createValidVariableName(
+//         name
+//       )}')">
+//         <p>${name}</p>
+//         <div class="addTask" onclick="addTask(event)">        
+//           <img src="/assets/icons/add.svg" alt="Добавить" />
+//         </div>
+//       </div>
+//       <div class="dropdown-content"></div>
+//     </div>
+//       `;
+//     });
+//     container.innerHTML = html;
+//   })
+//   .catch((err) => errorModal(err.message));
 
-function createValidVariableName(str) {
-  // Remove any non-alphanumeric characters
-  const cleanedStr = str.replace(/[^а-яА-Яa-zA-Z0-9_$]/g, "");
+// function createValidVariableName(str) {
+//   // Remove any non-alphanumeric characters
+//   const cleanedStr = str.replace(/[^а-яА-Яa-zA-Z0-9_$]/g, "");
 
-  // Ensure the first character is a letter, underscore, or dollar sign
-  let validName = cleanedStr.charAt(0).match(/[а-яА-Яa-zA-Z_$]/)
-    ? cleanedStr.charAt(0)
-    : "_";
+//   // Ensure the first character is a letter, underscore, or dollar sign
+//   let validName = cleanedStr.charAt(0).match(/[а-яА-Яa-zA-Z_$]/)
+//     ? cleanedStr.charAt(0)
+//     : "_";
 
-  // Replace any remaining spaces with underscores
-  validName += cleanedStr.slice(1).replace(/\s+/g, "_");
+//   // Replace any remaining spaces with underscores
+//   validName += cleanedStr.slice(1).replace(/\s+/g, "_");
 
-  return validName;
-}
+//   return validName;
+// }
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,192 @@
-// if (TG) {
-//   TG.expand();
-//   const tgUserId = TG.initDataUnsafe.user.id
-// }
+const TG = window.Telegram?.WebApp;
 
+const errorModal = (message) => {
+  const overlay = document.createElement("div");
+  overlay.classList.add("modal-overlay");
+  overlay.id = "modal-overlay";
+  const header = document.createElement("h2");
+  header.innerText = "Что-то пошло не так..";
+  const wrapper = document.createElement("div");
+  wrapper.appendChild(header);
+  wrapper.classList.add("modal-wrapper");
+  const container = document.createElement("p");
+  container.innerText = message;
+  wrapper.appendChild(container);
+  overlay.appendChild(wrapper);
+  overlay.addEventListener("click", (e) => _onModalDismiss(e, overlay));
+  document.body.appendChild(overlay);
+};
+
+const fields = [
+  { label: "Task Name:", type: "text", name: "taskName", required: true },
+  { label: "Task Description:", type: "textarea", name: "taskDescription" },
+  {
+    label: "Priority:",
+    type: "select",
+    name: "priority",
+    options: ["Low", "Medium", "High"],
+  },
+  { label: "End Date:", type: "date", name: "endDate" },
+];
+
+function toggleDropdown(category) {
+  const dropdown = document.getElementById(category);
+
+  dropdown.classList.toggle("open");
+}
+
+const showAddTaskModal = (children) => {
+  const overlay = document.createElement("div");
+  overlay.classList.add("modal-overlay");
+  overlay.id = "modal-overlay";
+  const header = document.createElement("h2");
+  header.innerText = "Создание новой задачи";
+  const wrapper = document.createElement("div");
+  wrapper.appendChild(header);
+  wrapper.classList.add("modal-wrapper");
+  const formContainer = document.createElement("div");
+  formContainer.classList.add("form-container");
+  const form = document.createElement("form");
+  form.classList.add("form-container");
+
+  fields.forEach((field) => {
+    const label = document.createElement("label");
+    label.innerText = field.label;
+
+    if (field.type === "textarea") {
+      const textarea = document.createElement("textarea");
+      textarea.name = field.name;
+      form.appendChild(label);
+      form.appendChild(textarea);
+    } else if (field.type === "select") {
+      const select = document.createElement("select");
+      select.name = field.name;
+
+      field.options.forEach((optionText) => {
+        const option = document.createElement("option");
+        option.value = optionText.toLowerCase();
+        option.text = optionText;
+        select.appendChild(option);
+      });
+
+      form.appendChild(label);
+      form.appendChild(select);
+    } else {
+      const input = document.createElement("input");
+      input.type = field.type;
+      input.name = field.name;
+      if (field.required) {
+        input.required = true;
+      }
+      form.appendChild(label);
+      form.appendChild(input);
+    }
+  });
+
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.innerText = "Добавить";
+  form.appendChild(submitButton);
+  formContainer.appendChild(form);
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(e);
+  });
+  //
+
+  wrapper.appendChild(formContainer);
+  overlay.appendChild(wrapper);
+  overlay.addEventListener("click", (e) => _onModalDismiss(e, overlay));
+  document.body.appendChild(overlay);
+};
+
+function addTask(e, status) {
+  e.stopPropagation();
+  showAddTaskModal();
+}
+const getter = async (url, body) => {
+  console.log({
+    user_id: TG.initDataUnsafe?.user?.id,
+    ...body,
+  });
+  return await fetch("https://innoglobalhack.site/api/" + url, {
+    method: "POST",
+    body: JSON.stringify({
+      user_id: TG.initDataUnsafe?.user?.id,
+      ...body,
+    }),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+getter("get_columns_by_user_id")
+  // .then((res) =>
+  //   res.ok && 0 ? res.json() : ["inWork", "done", "planned", "closed"]
+  // )
+  .then((data) => {
+    console.log("RESPONSE: ", data);
+    window.fetchedData = data;
+    const container = document.querySelector(".tasks");
+    let html = "";
+    data.forEach(({ name }) => {
+      html += `
+      <div class="dropdown category" id="${createValidVariableName(name)}">
+      <div class="dropdown-btn" onclick="toggleDropdown('${createValidVariableName(
+        name
+      )}')">
+        <p>${name}</p>
+        <div class="addTask" onclick="addTask(event)">        
+          <img src="/assets/icons/add.svg" alt="Добавить" />
+        </div>
+      </div>
+      <div class="dropdown-content"></div>
+    </div>
+      `;
+    });
+    container.innerHTML = html;
+    return data;
+  })
+  .then((data) => {
+    console.log("1: ", data);
+    for (const column of data) {
+      render_data(
+        column.tasks,
+        document.getElementById(createValidVariableName(column.name))
+      );
+    }
+  })
+  .catch((err) => errorModal(err.message))
+  .finally(() => {
+    setTimeout(() => {
+      const loader = document.querySelector(".loader");
+      loader.classList.add("hide");
+      setTimeout(() => {
+        loader.remove();
+      }, 200);
+    }, 1000);
+  })
+  .catch((err) => errorModal(err.message));
+
+function createValidVariableName(str) {
+  // Remove any non-alphanumeric characters
+  const cleanedStr = str.replace(/[^а-яА-Яa-zA-Z0-9_$]/g, "");
+
+  // Ensure the first character is a letter, underscore, or dollar sign
+  let validName = cleanedStr.charAt(0).match(/[а-яА-Яa-zA-Z_$]/)
+    ? cleanedStr.charAt(0)
+    : "_";
+
+  // Replace any remaining spaces with underscores
+  validName += cleanedStr.slice(1).replace(/\s+/g, "_");
+
+  return validName;
+}
+// fghkjlklk.kjgjfghvbjhj;lkufjkgcjhvkjblhklyktyjdgjvhbjhl;kuryjdjghcvjhlulfykjdghxmcvkjlfkjgdhmcvjglfhjkgcmbvbjlh;glfhjcgbbjlg;kh,jbn.blh;ogul
 const render_data = (data, ref) => {
   console.log("data: ", data);
   const container = document.createElement("div");
@@ -70,31 +254,30 @@ const _getDate = (stringDate) => {
   const dateString = `${day}.${month}.${year}`;
   return dateString;
 };
-window.addEventListener("load", () => {
-  // HERE!
+// window.addEventListener("load", () => {
+//   // HERE!
 
-  Promise.resolve(window.fetchedData)
-    .then((data) => {
-      console.log('1: ', data);
-      for (const column of data) {
-        console.log('col: ', column);
-        render_data(
-          column.tasks,
-          document.getElementById(createValidVariableName(column.name))
-        );
-      }
-    })
-    .catch((err) => errorModal(err.message))
-    .finally(() => {
-      setTimeout(() => {
-        const loader = document.querySelector(".loader");
-        loader.classList.add("hide");
-        setTimeout(() => {
-          loader.remove();
-        }, 200);
-      }, 1000);
-    });
-});
+//   Promise.resolve(window.fetchedData)
+//     .then((data) => {
+//       console.log("1: ", data);
+//       for (const column of data) {
+//         render_data(
+//           column.tasks,
+//           document.getElementById(createValidVariableName(column.name))
+//         );
+//       }
+//     })
+//     .catch((err) => errorModal(err.message))
+//     .finally(() => {
+//       setTimeout(() => {
+//         const loader = document.querySelector(".loader");
+//         loader.classList.add("hide");
+//         setTimeout(() => {
+//           loader.remove();
+//         }, 200);
+//       }, 1000);
+//     });
+// });
 
 // МОДАЛКА
 const _onModalDismiss = (e, overlay) => {
